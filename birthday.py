9db2e67c362fa36a80401c1db54dfa58c5bca6ff
Daniel Dias Pereira <86617987+DanielDPereira@users.noreply.github.com>
2023-05-15 21:03:18 -0300

    Update birthday.py

--- birthday.py
+++ birthday.py
@@ -2,4 +2,4 @@ import math as math
 
 X = int(input("Amount of people: "))
 
-print(f"A chance de {X} pessoas fazerem aniversário no mesmo dia é de " + (str(100 - (math.factorial(365)/(365 ** X * math.factorial(365 - X))) * 100))+"%")
+print(f"The chance that {X} people have the same birthday is " + (str(100 - (math.factorial(365)/(365 ** X * math.factorial(365 - X))) * 100))+"%")
